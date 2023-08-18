6fb7ce256b2fbccda6db9c1c9f4b60af44a7fe4c
TheFabi8A <cafaruoc2000@hotmail.com>
2023-08-18 18:34:48 -0500

    .

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <>
       <SliderPrimaryAnimes />
+      <iframe
+        width="560"
+        height="315"
+        src="https://streamwish.to/e/zwe08agupkgg"
+        allowfullscreen=""></iframe>
     </>
   );
 }
