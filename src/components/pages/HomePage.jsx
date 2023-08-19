08165be351b4fe97617bf5dab87b892cae01ac9c
TheFabi8A <cafaruoc2000@hotmail.com>
2023-08-19 02:21:37 -0500

    .

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <>
       <SliderPrimaryAnimes />
-      <iframe
-        width="560"
-        height="315"
-        src="https://streamwish.to/e/zwe08agupkgg"
-        allowfullscreen=""></iframe>
-      <iframe
-        src="https://vudeo.co/embed-oyfqg5l4cz60.html"
-        allowfullscreen=""></iframe>
     </>
   );
 }
